33ed630f09b9903d4162408dfac926fbd73a915f
Becky Bell <rlbell@alum.bu.edu>
2014-04-26 19:41:18 -0400

    initialized new git repository

---
+++ UrbanTiller/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urbantiller::Application.config.secret_key_base = '876b702d56f6053954a715cfbadd8f3ca8b81da4a2b030033e05765f82109be0d72164ce6a3226ea9579a1a7e68053292dcfca392acda8fbe1a3a016bad8fbd4'
